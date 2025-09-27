79be333fa784c9938954a27872ecb1e896d1bff7
ptg942 <ptg942@gmail.com>
2025-09-27 22:40:16 +0500

    Add project and deal schema

--- packages/data-access/src/lib/data-access.module.ts
+++ packages/data-access/src/lib/data-access.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import {MongooseModule} from "@nestjs/mongoose";
-import {User, UserSchema, Client, ClientSchema} from "./schema";
+import {User, UserSchema, Client, ClientSchema, Project, ProjectSchema, Deal, DealSchema} from "./schema";
 
 @Module({
 	controllers: [],
@@ -9,6 +9,8 @@ import {User, UserSchema, Client, ClientSchema} from "./schema";
 	imports: [ MongooseModule.forFeature([
 		{ name: User.name, schema: UserSchema },
 		{ name: Client.name, schema: ClientSchema },
+		{ name: Project.name, schema: ProjectSchema },
+		{ name: Deal.name, schema: DealSchema },
 	]),]
 })
 export class ArchCrmDataAccessModule {}
